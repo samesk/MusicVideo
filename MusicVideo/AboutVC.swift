4b46ba2f60730a43a0758b952f8f15645998ad6c
esamuel55@hotmail.com <esamuel55@hotmail.com>
2016-05-13 22:49:26 +0300

    couple Part 24A

---
+++ MusicVideo/AboutVC.swift
@@ -0,0 +1,37 @@
+//
+//  AboutVC.swift
+//  MusicVideo
+//
+//  Created by Samuel Eskenasy on 13/05/2016.
+//  Copyright © 2016 Samuel Eskenasy. All rights reserved.
+//
+
+import UIKit
+
+class AboutVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "About"
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
